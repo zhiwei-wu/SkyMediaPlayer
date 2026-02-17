1428be9b7c9453e180716097428a3c7e352aa9ed
吴志伟 <zhiwei.wu@foxmail.com>
2026-02-17 18:31:58 +0800

    fix: use standard <android/log.h> instead of hardcoded NDK path

--- skymediaplayer/src/main/cpp/include/logger.h
+++ skymediaplayer/src/main/cpp/include/logger.h
@@ -5,7 +5,7 @@
 extern "C" {
 #endif
 
-#include "../../../../../../../Library/Android/sdk/ndk/27.0.12077973/toolchains/llvm/prebuilt/darwin-x86_64/sysroot/usr/include/android/log.h"
+#include <android/log.h>
 
 #define ALOG_I(TAG, ...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
 #define ALOG_D(TAG, ...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
